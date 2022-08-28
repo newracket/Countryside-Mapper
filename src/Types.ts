6eaaac572e954c2cfbaf9da04956153094357810
newracket <newracket@users.noreply.github.com>
2022-08-27 22:41:22 -0700

    Fixed search bar

--- src/Types.ts
+++ src/Types.ts
@@ -1,18 +1,3 @@
-import { LatLngExpression } from "leaflet"
-
-export type LocationData = {
-    coords: LatLngExpression;
-    name: string;
-    host: string;
-    startDate: Date;
-    endDate: Date;
-    description: string;
-    imageLocation: string;
-    website: string;
-    email: string;
-    phone: string;
-}
-
 export interface ILocationData {
     id?: string | null,
     host: string,
